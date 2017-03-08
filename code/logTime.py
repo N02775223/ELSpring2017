5cf38707207db2d3fa97a675542085c196cd80e0
N02775223 <n02775223@gmail.com>
2017-03-08 19:28:04 +0000

    Update logTime.py

--- code/logTime.py
+++ code/logTime.py
@@ -4,9 +4,9 @@ import sqlite3 as mydb
 import sys
 import datetime
 #Author@ Jason Goodman
-def readTime():
+def logTime():
     xdate = time.strftime('%Y-%m-%d') #Year-Month-Day, ex: '2017-03-08'
     xtime = time.strftime('%H-%M-%S') #Hour-Minute-Second, ex: '18-59-17'
     return [xdate, xtime]
-print readTime()
+#print readTime()
 
